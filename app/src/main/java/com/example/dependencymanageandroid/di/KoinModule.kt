2e5ad43882cd819ec59d4a7e220e82943681d386
Hossain MuktaR <HossainMuktarDev@gmail.com>
2023-09-20 04:23:28 +0600

    Koin module: classic or constructor DSL

--- app/src/main/java/com/example/dependencymanageandroid/di/KoinModule.kt
+++ app/src/main/java/com/example/dependencymanageandroid/di/KoinModule.kt
@@ -6,13 +6,20 @@ import com.example.dependencymanageandroid.domain.UserListUseCase
 import com.example.dependencymanageandroid.domain.UserRepository
 import com.example.dependencymanageandroid.presentation.UserViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 object KoinModule {
     val appModule = module {
-        single<DemoApi> { DemoApi() }
-        single<UserRepository> { UserRepositoryImpl(get()) }
-        single<UserListUseCase> { UserListUseCase(get()) }
-        viewModel { UserViewModel(get()) }
+//        single<DemoApi> { DemoApi() }
+        singleOf(::DemoApi)
+//        single<UserRepository> { UserRepositoryImpl(get()) }
+        singleOf(::UserRepositoryImpl) { bind<UserRepository>()}
+//        single<UserListUseCase> { UserListUseCase(get()) }
+        singleOf(::UserListUseCase)
+//        viewModel { UserViewModel(get()) }
+        viewModelOf(::UserViewModel)
     }
 }
